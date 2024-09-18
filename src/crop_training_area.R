82bc1bc971490eafaca9eec76474b45bb2e3cd19
Ana Maritza Bello <amby.121192@gmail.com>
2024-09-18 16:26:43 +0100

    💥 Add functions to crop and stack rasters

--- src/crop_training_area.R
+++ src/crop_training_area.R
@@ -1,38 +1,48 @@
 library(terra)
-library(raster)
 library(sf)
 
-load_raster <- function(file_name) {
-  raster(file.path(raster_dir, file_name))
+# Función para cargar rásteres
+load_raster <- function(raster_dir, file_name) {
+  rast(file.path(raster_dir, file_name))
 }
 
-load_polygon <- function(file_name) {
-  st_read(file.path(file_name))
+# Función para cargar el polígono del área calibración (convex hull)
+load_polygon <- function() {
+  convex_hull_path <- "data/binary/convex_hull.shp"
+  st_read(convex_hull_path)
 }
 
-main_dir <- ""
-raster_dir <- "resampled_data/2014/01/"
-rasters <- c(
-  sst = "sst_resampled.tif",
-  chlc = "chlor_a_resampled.tif",
-  eastward_wind = "wind_speed.tif",
-  northward_wind = "wind_direction.tif"
-)
+# Función para crear el stack de rásteres
+create_raster_stack <- function(raster_dir, rasters) {
+  loaded_rasters <- lapply(
+    rasters, function(raster_file) load_raster(raster_dir, raster_file)
+    )
+  env_stack <- rast(loaded_rasters)
+  return(env_stack)
+}
 
-loaded_rasters <- lapply(rasters, load_raster)
-polygon <- load_polygon("data/binary/convex_hull.shp")
+# Función para recortar y enmascarar el ráster
+crop_and_mask_raster <- function(env_stack, polygon, raster_names) {
+  training_raster <- crop(env_stack, polygon)
+  masked_raster <- mask(training_raster, polygon)
+  names(masked_raster) <- raster_names
+  return(masked_raster)
+}
 
-env_stack <- stack(loaded_rasters)
-env_stack
+# Función para generar el ráster recortado y enmascarado
+generate_masked_raster <- function(year, month) {
+  raster_dir <- file.path("resampled_data", year, month)
+  
+  rasters <- c(
+    sst = "sst_resampled.tif",
+    chlc = "chlor_a_resampled.tif",
+    eastward_wind = "wind_speed.tif",
+    northward_wind = "wind_direction.tif"
+  )
 
-names(env_stack) <- names(rasters)
-
-print(names(env_stack))
-
-training_raster <- crop(env_stack, polygon)
-
-masked_raster <- mask(training_raster, polygon)
-
-# Save the masked raster
-writeRaster(masked_raster, "training_env.tif", format = "GTiff", overwrite = TRUE)
-
+  polygon <- load_polygon()
+  
+  env_stack <- create_raster_stack(raster_dir, rasters)
+  masked_raster <- crop_and_mask_raster(env_stack, polygon, names(rasters))
+  return(masked_raster)
+}
