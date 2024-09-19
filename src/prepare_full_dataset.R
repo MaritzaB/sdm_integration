9a983beef9d9ad1689a2f33f88db0ce87be7856a
Ana Maritza Bello <amby.121192@gmail.com>
2024-09-19 20:31:47 +0100

    🚚 Move functions to their respective folder and create a workflow in src

---
+++ src/prepare_full_dataset.R
@@ -0,0 +1,52 @@
+library(dismo)
+library(terra)
+library(raster)
+library(biomod2)
+library(dplyr)
+source("functions/process_raster_data.R")
+source("functions/process_occurrence_data.R")
+source("functions/generate_pseudo_absences.R")
+
+# Definir la lista de tuplas con año y mes específicos
+year_month_list <- list(
+  c("2014", "01"), c("2014", "02"), c("2014", "03"), c("2014", "04"), c("2014", "05"), c("2014", "12"),
+  c("2015", "01"), c("2015", "02"), c("2015", "03"),
+  c("2016", "02"), c("2016", "03"), c("2016", "04"),
+  c("2017", "02"), c("2017", "03"),
+  c("2018", "01"), c("2018", "02")
+)
+
+
+full_ml_dataset <- data.frame()
+
+# Iterar sobre la lista de tuplas para generar el dataset completo
+for (year_month in year_month_list) {
+  year <- year_month[1]
+  month <- year_month[2]
+
+  temp_dataset <- tryCatch({
+    create_modeling_dataset(year, month)
+  }, error = function(e) {
+    cat("Error al procesar year:", year, "month:", month, "\n")
+    return(NULL)
+  })
+
+  if (!is.null(temp_dataset)) {
+    full_ml_dataset <- rbind(full_ml_dataset, temp_dataset)
+  }
+}
+
+
+
+print(dim(full_ml_dataset))
+print(head(full_ml_dataset))
+
+
+output_directory <- "model_dataset"
+if (!dir.exists(output_directory)) {
+  dir.create(output_directory, recursive = TRUE)
+}
+
+output_file <- file.path(output_directory, "full_ml_dataset.csv")
+write.csv(full_ml_dataset, file = output_file, row.names = FALSE)
+cat("Proceso completado. Archivo CSV guardado en:", output_file, "\n")
