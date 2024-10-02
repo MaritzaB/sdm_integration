aae03f5fd14caa72f26b86d8b51c56e2a98e5976
Ana Maritza Bello <amby.121192@gmail.com>
2024-10-02 20:36:24 +0100

    🔨 tentativamente balance presences and absences per season

---
+++ functions/balance_presence_absence.R
@@ -0,0 +1,81 @@
+ignore <- suppressPackageStartupMessages({
+  library(dplyr)
+})
+
+get_data_directories <- function(number_of_variables) {
+  if (number_of_variables == 4) {
+    absence_dir <- "model_dataset_4vars/"
+    presence_dir <- "presence_data_4v/"
+  } else if (number_of_variables == 2) {
+    absence_dir <- "model_dataset_2vars/"
+    presence_dir <- "presence_data_2v/"
+  } else {
+    stop("El número de variables no es válido. Debe ser 2 o 4.")
+  }
+  return(list(absence_dir = absence_dir, presence_dir = presence_dir))
+}
+
+# Función para renombrar las columnas del archivo de ausencias
+rename_absence_columns <- function(absence_data) {
+  absence_data <- absence_data %>%
+    rename(
+      longitude = x,   # Cambiar 'x' a 'longitude'
+      latitude = y,    # Cambiar 'y' a 'latitude'
+      phoebastria_immutabilis = species_data  # Cambiar 'species_data' a 'phoebastria_immutabilis'
+    )
+  return(absence_data)
+}
+
+# Función para filtrar los datos por año y mes
+filter_by_year_month <- function(data, year, month) {
+  data %>%
+    filter(nyear == year, nmonth == month)
+}
+
+# Función para tomar una muestra balanceada de ausencias
+sample_absences <- function(absences_data, num_presences, sample_ratio = 1.2) {
+  sample_size <- ceiling(num_presences * sample_ratio)
+  num_absences <- nrow(absences_data)
+  
+  if (num_absences > sample_size) {
+    absences_sampled <- absences_data %>%
+      sample_n(sample_size)
+  } else {
+    absences_sampled <- absences_data
+  }
+  
+  return(absences_sampled)
+}
+
+# Función para balancear los datos por año y mes
+balance_data_for_one_month_year <- function(presences_data, absences_data, year, month, sample_ratio = 1.2) {
+  presences_filtered <- filter_by_year_month(presences_data, year, month)
+  absences_filtered <- filter_by_year_month(absences_data, year, month)
+  num_presences <- nrow(presences_filtered)
+
+  absences_filtered <- rename_absence_columns(absences_filtered)
+  
+  absences_sampled <- sample_absences(absences_filtered, num_presences, sample_ratio)
+  combined_data <- bind_rows(presences_filtered, absences_sampled)
+  
+  return(combined_data)
+}
+
+# Función principal para balancear los datos por mes y año
+balance_data_by_month_year <- function(presences_data, absences_data, sample_ratio = 1.2) {
+  unique_year_months <- presences_data %>%
+    select(nyear, nmonth) %>%
+    distinct()
+  
+  final_dataset <- data.frame()
+  
+  for (i in 1:nrow(unique_year_months)) {
+    year <- unique_year_months$nyear[i]
+    month <- unique_year_months$nmonth[i]
+    
+    balanced_data <- balance_data_for_one_month_year(presences_data, absences_data, year, month, sample_ratio)
+    final_dataset <- bind_rows(final_dataset, balanced_data)
+  }
+  
+  return(final_dataset)
+}
---
+++ src/join_presence_absence.R
@@ -0,0 +1,51 @@
+ignore <- suppressPackageStartupMessages({
+  library(dplyr)
+  source("functions/balance_presence_absence.R")
+})
+
+presence_absence_files <- list(
+    train_crianza = list(
+        presence = "train_crianza.csv",
+        absence = "absence_train_crianza.csv"
+    ),
+    test_crianza = list(
+        presence = "test_crianza.csv",
+        absence = "absence_test_crianza.csv"
+    ),
+    train_empollamiento = list(
+        presence = "train_empollamiento.csv",
+        absence = "absence_train_empollamiento.csv"
+    ),
+    test_empollamiento = list(
+        presence = "test_empollamiento.csv",
+        absence = "absence_test_empollamiento.csv"
+    ),
+    train_incubacion = list(
+        presence = "train_incubacion.csv",
+        absence = "absence_train_incubacion.csv"
+    ),
+    test_incubacion = list(
+        presence = "test_incubacion.csv",
+        absence = "absence_test_incubacion.csv"
+    )
+)
+
+n_vars <- 4
+dirs <- get_data_directories(n_vars)
+
+absence_dir <- dirs$absence_dir
+presence_dir <- dirs$presence_dir
+out_dir <- "presence_absence/"
+
+if (!dir.exists(out_dir)) {
+  dir.create(out_dir, recursive = TRUE)
+}
+
+for (name in names(presence_absence_files)) {
+  presence_data <- read.csv(paste0(presence_dir, "/", presence_absence_files[[name]]$presence))
+  absence_data <- read.csv(paste0(absence_dir, "/", presence_absence_files[[name]]$absence))
+  out_file <- paste0(out_dir, name, ".csv")
+  balanced_dataset <- balance_data_by_month_year(presence_data, absence_data, sample_ratio = 1.2)
+  write.csv(balanced_dataset, out_file, row.names = FALSE)
+  cat("Archivo balanceado creado:", out_file, "\n")
+}
