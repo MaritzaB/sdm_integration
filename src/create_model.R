9177373bbe39cf29db85e9e467cc9c15e2823915
Ana Maritza Bello <amby.121192@gmail.com>
2024-10-09 18:47:09 +0100

    🔊 set models to implement

--- src/create_model.R
+++ src/create_model.R
@@ -18,33 +18,19 @@ n_vars <- 4
 biomod_crianza <- get_biomod_data_object("incubacion", n_vars)
 biomod_crianza
 
-cv.k <- bm_CrossValidation(bm.format = biomod_crianza,
-  strategy = 'kfold',
-  nb.rep = 5,
-  k = 10)
-
-print(dim(cv.k))
-
-
-opt.df <- bm_ModelingOptions(data.type = 'binary',
-  models = c('RF'),
-  strategy = 'default',
-  bm.format = biomod_crianza,
-  calib.lines = cv.k)
+single_models <- c('GLM', 'GAM', 'GBM','MARS', 'RF')
 
 myBiomodelOut <- BIOMOD_Modeling(
   bm.format = biomod_crianza,
   modeling.id = 'incubacion',
-  models = c('RF'),
-  CV.strategy = 'kfold',
-  CV.nb.rep = 5,
-  CV.k = 10,
-  OPT.strategy = 'tuned',
-  metric.eval = c('TSS', 'ROC'),
+  models = single_models,
+  CV.strategy = 'strat',
+  CV.nb.rep = 10,
+  CV.k = 5,
+  CV.balance = 'presences',
+  CV.strat = 'x',
+  OPT.strategy = 'bigboss',
+  metric.eval = c('TSS', 'ROC', 'ACCURACY'),
   var.import = 4,
   seed.val = 42)
-
-myCalibLines <- get_calib_lines(myBiomodelOut)
-myCalibLines
-#plot(biomod_crianza, calib.lines = myCalibLines)
-  
+ 
