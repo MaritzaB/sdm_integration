ef9d71a445641a23d97fc11832c776c5afcf40ef
maritzée <amby.121192@gmail.com>
2024-11-28 18:56:31 -0600

    📈 Update plots

--- src/total_data_plot.R
+++ src/total_data_plot.R
@@ -11,11 +11,20 @@ meses <- c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Agosto", "Dic
 # Crear el gráfico coloreando por mes con la paleta Set2
 ggplot(data, aes(x = year_month, y = total_puntos, fill = as.factor(mes))) +
   geom_col(color = "black") +  # Gráfico de barras
+  geom_text(aes(label = total_puntos), 
+            position = position_stack(vjust = 0.5),  # Posicionar en el centro de la barra
+            color = "white", size = 3) +  # Color de las etiquetas
   labs(title = "Total de Puntos por Año y Mes", 
        x = "Año-Mes", 
        y = "Total Puntos", 
        fill = "Mes") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +  # Rotar etiquetas del eje X
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Rotar etiquetas del eje X
+    legend.position = c(0.95, 0.7),  # Posicionar la leyenda (x, y) dentro del gráfico
+    legend.background = element_rect(fill = "white", color = "black"),  # Fondo blanco para la leyenda
+    legend.title = element_text(size = 10),  # Ajustar tamaño del título de la leyenda
+    legend.text = element_text(size = 8)  # Ajustar tamaño del texto de la leyenda
+  ) +
   scale_fill_brewer(palette = "Set2", labels = meses) +  # Cambiar etiquetas por nombres de meses
   scale_y_continuous(labels = scales::comma, breaks = seq(0, max(data$total_puntos, na.rm = TRUE), by = 20000))  # Ajustar eje Y
 
@@ -24,4 +33,4 @@ if (!dir.exists("figures")) {
   dir.create("figures")
 }
 
-ggsave("figures/total_puntos_coloreado_por_mes.png", width = 12, height = 6)
+ggsave("figures/total_puntos_por_mes.png", width = 12, height = 6)
